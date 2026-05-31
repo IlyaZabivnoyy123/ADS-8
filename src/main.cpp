1d9ce00f2fdbbd4377cf3b7f46795e52906c77cd
IlyaZabivnoyy123 <ilyameiplay@gmail.com>
2026-05-31 23:25:11 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 // Copyright 2021 NNTU-CS
+
+#include <iostream>
+#include <string>
+
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
 int main() {
+    BST<std::string> dictionary;
+
+    makeTree(dictionary, "src/war_peace.txt");
+    printFreq(dictionary);
+
+    std::cout << dictionary.depth() << std::endl;
+
     return 0;
 }
